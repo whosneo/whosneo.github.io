f103da5ea80a0cde0ffb9fbb2b737e1c96fa0ff1
real-neo <questionyugood@gmail.com>
2019-12-31 19:20:21 +0800

    Sync with upstream

--- html_files/main.js
+++ html_files/main.js
@@ -2,14 +2,14 @@
  * Created by Noisky on 17/05/13.
  * Revised by Noisky on 19/09/10.
  */
-$(document).ready(function () {
+$(document).ready(function() {
     /**
      * 随机获取背景图片
      */
     var bgNum = 10; // 定义随机数范围 1-10 和图片数量保持一致
     var randomNum = Math.floor(Math.random() * bgNum) + 1;
     // 拼接图片地址
-    var imgUrl = 'https://cdn.jsdelivr.net/gh/noisky/Homepage@master/Noisky_files/img/background-image/bg-' + randomNum + '.jpg';
+    var imgUrl = './html_files/img/background-image/bg-' + randomNum + '.jpg';
     // 替换页面中的背景图片地址
     $("header").css("background-image", "url(" + imgUrl + ")");
 
@@ -20,15 +20,14 @@ $(document).ready(function () {
 
     // 定义菜单关闭事件
     function bounceOutUp() {
-        nw.on(function () {
+        nw.on(function() {
             nw.toggleClass('visible animated bounceOutUp');
-
         });
         nw.toggleClass('animated bounceInDown animated bounceOutUp');
     }
 
     // 根据菜单状态定义单击的操作
-    $('.btn-mobile-menu').click(function () {
+    $('.btn-mobile-menu').click(function() {
         if (nw.css('display') === "block") {
             bounceOutUp();
         } else {
@@ -49,7 +48,7 @@ $(document).ready(function () {
         $('.btn-mobile-menu_icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
     });
     // 点击下拉菜单以外的其他标签区域收起菜单生效
-    $(".panel-main").on('click', ':not(.mobile,.btn-mobile-menu,.navigation-wrapper)', function () {
+    $(".panel-main").on('click', ':not(.mobile,.btn-mobile-menu,.navigation-wrapper)', function() {
         if (nw.hasClass("bounceInDown")) {
             bounceOutUp();
             $('.btn-mobile-menu_icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
@@ -59,7 +58,7 @@ $(document).ready(function () {
         $('.navigation-wrapper').css('position') === "absolute" ? $('.panel-main').css('position', 'fixed') : ""
     });
     // 阻止冒泡事件执行
-    nw.click(function (event) {
+    nw.click(function(event) {
         event.stopPropagation();
     });
     /**
@@ -78,10 +77,10 @@ $(document).ready(function () {
             async: true,
             jsonp: "callback",
             jsonpCallback: "hitokoto",
-            success: function (result) {
+            success: function(result) {
                 $('#hitokoto').html("<p>" + result.hitokoto + "</p>")
             },
-            error: function () {
+            error: function() {
                 $('#hitokoto').html("<p>读取数据失败了的说……_(:з」∠)_</p>")
             }
         });
